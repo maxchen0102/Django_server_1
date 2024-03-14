f380ce05330015ff77acd6b9e08808dd2eaaa96a
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-14 15:25:17 +0800

    first

---
+++ api/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render
+import json 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from product.models import Product
+from django.forms.models import model_to_dict
+# Create your views here.
+
+
+# def api_home(request): 
+    
+#     try :
+#         params = request.GET.dict()
+#         print(params)
+#         print("connect success")
+        
+#     except:
+#         pass
+#     print(request.POST)
+#     print(request.GET)
+
+    
+   
+#     return  JsonResponse({"status": "GET success"})
+
+# @csrf_exempt
+# def api_post(request):
+#     if request.method == 'POST':
+#         # Handle POST request with JSON data
+#         try:
+#             json_data = json.loads(request.body)
+#             # Process JSON data here
+#             print(json_data)
+#             return JsonResponse({'status': 'POST success'})
+#         except json.JSONDecodeError:
+#             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+#     else:
+#         # Handle other HTTP methods (e.g., GET)
+#         return JsonResponse({'error': 'Unsupported method'}, status=405)
+    
+#取得資料by id
+
+
+def test(request):
+    data={"status":"ok"}
+    return JsonResponse(data)
+def get_product(request,id):
+    print("this")
+    if id :
+        product=Product.objects.get(id=id)
+        data = model_to_dict(product)
+        print(data)
+        return JsonResponse(data)
+
+#取得全部資料
+def get_all_product(request):
+    print("no2123123132")
+    data=Product.objects.all().values()
+    print(data)
+    final_data={}
+    for item in data :
+        final_data={ item['id']:item  for item in data }
+    print("connect success")
+    return JsonResponse(final_data)
+
+#新增資料
+@csrf_exempt
+def add_product(reqeust): 
+    json_data = json.loads(reqeust.body)
+    print(json_data)
+    try : 
+        new_product=Product(**json_data)
+        new_product.save()
+        return JsonResponse({'status': 'POST success'})
+    except : 
+        return JsonResponse({'error': 'Invalid JSON data'})
+
+#刪除資料
+@csrf_exempt
+def delete_prodcut_by_id(request, product_id):
+    if request.method == 'DELETE':
+        try:
+            product = Product.objects.get(id=product_id)
+            product.delete()
+            return JsonResponse({'status': 'success'})
+        except Product.DoesNotExist:
+            return JsonResponse({'error': 'no oooooooProduct not found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+        
+#更新資料
+@csrf_exempt
+def product_update(request,id):
+    data=json.loads(request.body)
+    print(data)
+    print(data)
+    print("the id is ",id)
+    print(type(id))
+    if request.method == 'PUT':
+        try : 
+            product = Product.objects.get(id=id)
+            print(product)
+            product.title=data["title"]
+            product.conttne=data['conttne']
+            product.price=data['price']
+            product.save()
+            
+            return JsonResponse({'status': 'updata success'})
+        except : 
+            return JsonResponse({'error': 'Invalid id '})
+    return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
---
+++ user.py
@@ -0,0 +1,41 @@
+import requests
+import json
+from requests import ConnectionError, HTTPError, Timeout
+
+try:
+    url = "http://localhost:8000/api"
+    url_get_product = "http://localhost:8000/api/product"
+    url_post_product = "http://localhost:8000/api/product_add"
+    url_prodcut_delete = "http://localhost:8000/api/prodcut_delete"
+    url_update_product = "http://localhost:8000/api/update_product"
+
+    params = {
+        'id': 12,
+        "name": "chris",
+        "flag": 2
+    }
+    json = {
+        "title": "moto",
+        "conttne": "cool but not cool ",
+        "price": 2500
+    }
+    password = {
+        "account": "kingcall",
+        "password": "1111"
+    }
+    json_update = {
+        "id": 2,
+        "title": "motoxxxxxxxxxx",
+        "conttne": "cool but not cool ",
+        "price": 2500999
+    }
+
+    # res = requests.post(url_post,json=json)
+    res = requests.post(url_update_product, params={"id": 2}, json=json_update)
+    print(res),
+    data = res.json()
+    print(data)
+
+except ConnectionError:
+    print('找不到伺服器')
+    print('沒有發生問題')
