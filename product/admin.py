f4cf76d42a2f415e0a43a6c5121d86b5b9fed8ed
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-25 11:25:48 +0800

    完成product serializer 建立

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product2,Category
+
+admin.site.register(Product2)
+admin.site.register(Category)
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import  Product2
+from rest_framework.views import APIView
+
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products2 = Product2.objects.all()[0:4]
+        serializer = ProductSerializer(products2, many=True)
+        return Response(serializer.data)
