b897479131ce10435a288dadc93a9623d00fb702
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-25 16:06:43 +0800

    新增產品細節頁面

--- product/views.py
+++ product/views.py
@@ -6,10 +6,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import  Product2
 from rest_framework.views import APIView
-
+from django.http import Http404
 
 class LatestProductsList(APIView):
     def get(self, request, format=None):
         products2 = Product2.objects.all()[0:4]
         serializer = ProductSerializer(products2, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product2.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product2.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
